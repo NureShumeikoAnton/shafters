45e89a685330e920a842569fd4ee1054ab281022
Anton Shimeiko <shumejko.sasha@gmail.com>
2026-01-06 14:07:50 +0200

    feat: Connect inventory to Player (hotbar, use, drop)

--- player.gd
+++ player.gd
@@ -12,17 +12,45 @@ var flashlight_on = true
 # Generator spawning
 var generator_scene = preload("res://props/generator/generator.tscn")
 var lamp_scene = preload("res://props/lamp/lamp.tscn")
+var pickup_scene = preload("res://items/pickup/pickup.tscn")
 
 @onready var flashlight = $FlashlightPointLight2D
 @onready var flashlight2 = $FlashlightPointLight2D2
 @onready var power_bar = $CanvasLayer/MarginContainer/VBoxContainer/ProgressBar
+@onready var inventory: Inventory = $Inventory
+@onready var inventory_ui = $CanvasLayer/InventoryUI
 
 func _ready():
 	var bleed := Bleed.new(21)
 	$Limbs/Lleg.add_affliction(bleed)
 	affliction_ui = get_tree().current_scene.get_node("CanvasLayer/AfflictionsUI")
+	inventory_ui.set_inventory(inventory)
+	inventory_ui.item_triggered.connect(_on_ui_item_triggered)
+	inventory_ui.item_dropped.connect(_on_ui_item_dropped)
 	queue_redraw()
 	update_flashlight()
+	var test_gen = load("res://items/resources/generator_item.tres")
+	var test_lamp = load("res://items/resources/lamp_item.tres")
+	var test_chest = load("res://items/resources/chest_item.tres")
+	if test_gen:
+		inventory.add_item(test_gen)
+		print("Test: Generator added to inventory.")
+	else:
+		print("Test: Failed to load generator item.")
+
+	if test_lamp:
+		inventory.add_item(test_lamp)
+		inventory.add_item(test_lamp)  # Add two lamps for testing
+		print("Test: Lamp added to inventory.")
+	else:
+		print("Test: Failed to load lamp item.")
+
+	if test_chest:
+		inventory.add_item(test_chest)
+		print("Test: Chest added to inventory.")
+	else:
+		print("Test: Failed to load chest item.")
+	
 
 func _physics_process(delta):
 	# Handle flashlight power
@@ -95,7 +123,6 @@ func spawn_lamp():
 	print("Lamp spawned at: ", lamp.global_position)
 	
 func _input(event: InputEvent):
-	
 	# Spawn generator with "g" key
 	if event is InputEventKey and event.pressed and event.keycode == KEY_G:
 		spawn_generator()
@@ -107,3 +134,76 @@ func _input(event: InputEvent):
 	if event.is_action_pressed("HealthUI"):
 		if affliction_ui:
 			affliction_ui.toggle_for_player(self)
+
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_1: use_quick_slot(0)
+		elif event.keycode == KEY_2: use_quick_slot(1)
+		elif event.keycode == KEY_3: use_quick_slot(2)
+		elif event.keycode == KEY_4: use_quick_slot(3)
+
+# Вызывается при нажатии клавиш 1-4
+func use_quick_slot(index: int):
+	use_slot(index)
+
+# Вызывается при ЛКМ в инвентаре
+# Обрати внимание: теперь принимаем SlotData, но используем индекс
+func _on_ui_item_triggered(slot_data: SlotData, index: int):
+	use_slot(index)
+
+# Вызывается при ПКМ в инвентаре
+func _on_ui_item_dropped(index: int):
+	drop_item(index)
+
+# Основная логика использования предмета по индексу слота
+func use_slot(index: int):
+	# Получаем данные слота, а не просто предмет
+	var slot_data = inventory.get_slot_data(index)
+	if slot_data == null: return
+	
+	var item = slot_data.item_data
+	
+	if item is PlaceableItem:
+		# Если это строительный предмет
+		_spawn_placeable(item.placeable_scene)
+		
+		# Уменьшаем количество на 1 (тратим предмет)
+		inventory.decrease_item_at(index, 1)
+		
+	elif item is ConsumableItem:
+		# Если это расходник
+		item.use(self) 
+		
+		# Уменьшаем количество на 1
+		inventory.decrease_item_at(index, 1)
+
+# Спавн объекта в мире (без изменений)
+func _spawn_placeable(scene: PackedScene):
+	if scene == null: return
+	
+	var instance = scene.instantiate()
+	instance.global_position = global_position + Vector2(0, 50)
+	get_parent().add_child(instance)
+	
+	print("Построен объект: ", instance.name)
+
+# Выбрасывание предмета (целым стеком)
+func drop_item(index: int):
+	var slot_data = inventory.get_slot_data(index)
+	if slot_data == null: return
+	
+	var pickup = pickup_scene.instantiate()
+	# Передаем данные о типе предмета в Pickup
+	pickup.item_data = slot_data.item_data
+	
+	# Позиция со случайным смещением
+	var random_offset = Vector2(randf_range(-30, 30), randf_range(-30, 30))
+	pickup.global_position = global_position + random_offset
+	
+	# СНАЧАЛА добавляем в сцену
+	get_parent().add_child(pickup)
+	
+	# ПОТОМ включаем задержку подбора
+	pickup.disable_pickup_temporarily(0.7)
+	
+	# Удаляем ВЕСЬ слот из инвентаря (выбрасываем всю пачку)
+	inventory.remove_slot_at(index)
